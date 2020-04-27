5fc1f6239a608df0c866e7c84424051439d4311b
Philippe Massicotte <pmassicotte@hotmail.com>
2020-04-27 11:00:29 -0400

    Add bathymetry to the map.

--- R/fig01.R
+++ R/fig01.R
@@ -4,7 +4,10 @@
 # DESCRIPTION:
 #
 # Plot of the stations positions.
+# Bathymetry data: https://download.gebco.net/
 # <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+library(raster)
 
 rm(list = ls())
 
@@ -48,6 +51,23 @@ river_network <-
     ymax = 72.5
   ))
 
+# Prepare bathymetry data -------------------------------------------------
+
+bathy <- raster::raster("data/raw/bathymetry/GEBCO_2019_27_Apr_2020_5e98c581281a/gebco_2019_n75.0_s68.0_w-145.0_e-120.0.tif") %>%
+  # raster::sampleRegular(size = 1e4, asRaster = TRUE) %>%
+  raster::rasterToPoints() %>%
+  as_tibble() %>%
+  rename(z = 3) %>%
+  filter(between(y, 69, 73))
+
+bathy_interpolated <- bathy %>%
+  mba.surf(no.X = 600, no.Y = 600, sp = TRUE) %>%
+  as.data.frame() %>%
+  as_tibble() %>%
+  mutate(xyz.est.z = ifelse(xyz.est.z >= 0, 0, xyz.est.z))
+
+range(bathy_interpolated$xyz.est.z)
+
 # Plot --------------------------------------------------------------------
 
 set.seed(123)
@@ -59,6 +79,28 @@ set.seed(123)
 # station <- station %>% add_count(station, sort = TRUE) %>% filter(n > 1)
 
 p <- ggplot() +
+  ggisoband::geom_isobands(
+    data = bathy_interpolated,
+    aes(xyz.est.x, xyz.est.y, fill = xyz.est.z, z = xyz.est.z),
+    bins = 20, color = NA
+  ) +
+  paletteer::scale_fill_paletteer_c(
+    "ggthemes::Blue",
+    direction = -1,
+    limits = c(-4000, 0),
+    oob = scales::squish,
+    guide = guide_legend(
+      label.position = "top",
+      title = "Depth (m)",
+      title.position = "top",
+      title.hjust = 0.5,
+      title.theme = element_text(face = "bold", size = 8),
+      label.theme = element_text(size = 6),
+      keyheight = unit(0.25, "cm"),
+      keywidth = unit(1, "cm")
+    ),
+    breaks = -c(0, 1000, 2000, 3000)
+  ) +
   geom_sf(data = ne_land, size = 0.15) +
   geom_sf(data = river_network, size = 0.01, color = "gray75") +
   coord_sf(xlim = c(-141, -125), ylim = c(68.5, 72.5)) +
@@ -66,7 +108,7 @@ p <- ggplot() +
     x = longitude,
     y = latitude,
     color = factor(transect)
-  )) +
+  ), show.legend = FALSE) +
   geom_curve(
     data = arrow,
     aes(
@@ -116,7 +158,16 @@ p <- ggplot() +
   theme(
     axis.title = element_blank(),
     panel.grid = element_blank(),
-    legend.position = "none"
+    legend.key = element_blank(),
+    legend.text = element_text(size = 6),
+    legend.title = element_text(size = 7),
+    legend.key.size = unit(0.25, "cm"),
+    legend.margin = margin(t = 0, unit = "cm"),
+    legend.direction = "horizontal",
+    legend.position = c(0.99, 0.1),
+    legend.justification = c(1, 0),
+    legend.background = element_rect(fill = "transparent"),
+    panel.background = element_rect(fill = "#B9DDF1")
   ) +
   paletteer::scale_color_paletteer_d("ggsci::default_nejm")
 
@@ -133,3 +184,5 @@ ggsave(
 )
 
 knitr::plot_crop(destfile)
+
+detach("package:raster", unload = TRUE)
